cfcb048584f94c1790b67c6100858e38a2faac52
Julien Verneuil <grz.zrg@gmail.com>
2017-12-10 02:51:57 +0100

    update README improve granular synthesis

--- main.c
+++ main.c
@@ -238,7 +238,8 @@ static int paCallback( const void *inputBuffer, void *outputBuffer,
                                 smp = &samples[n->smp_index];
 
                                 float grain_start = (float)smp->frames;
-                                grain_start = roundf(grain_start * (fmaxf(fminf(n->alpha, 1.0f), 0.0f) * (randf(0.0f, 1.0f) * fabs(floorf(fminf(n->alpha - 0.0001f, 1.0f))))));
+                                float a = fabs(n->alpha);
+                                grain_start = roundf(grain_start * fmaxf(fminf(a, 1.0f), 0.0f) * (1.0f - randf(0.0f, 1.0f) * floorf(fminf(a - 0.0001f, 1.0f))));
 
                                 gr->index[k] = grain_start;
                                 gr->frames[k] = grain_start + fmaxf(randf(GRAIN_MIN_DURATION + chn_settings->gmin_size, chn_settings->gmax_size), GRAIN_MIN_DURATION) * (smp->frames - grain_start);

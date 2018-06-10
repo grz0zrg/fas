73ac2fe451f83ce5e7ba67f79774f85f1150073b
Julien Verneuil <grz.zrg@gmail.com>
2018-06-11 00:27:45 +0200

    fix granular divide by zero issue

--- note.c
+++ note.c
@@ -143,7 +143,7 @@ void fillNotesBuffer(unsigned int samples_count, unsigned int waves_count, unsig
                 _note->blue = blue;
 
                 _note->density = fabs(round(blue));
-                _note->norm_density = 1.0f / _note->density;
+                _note->norm_density = 1.0f / (_note->density + 0.0000001f);
 
                 if (_note->density < 1) {
                     _note->density = 1;
